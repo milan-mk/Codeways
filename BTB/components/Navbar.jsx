84b54d8ab9f742c59bfcec07c364c3a2cdaa5d48
milan-mk <milanmodak2005@gmail.com>
2025-01-10 23:54:00 +0530

    Added Navbar Component

---
+++ BTB/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css';
+
+
+const Navbar = () => {
+  return (
+    
+    <>
+    <nav class="navbar">
+        <div>
+            <h3 class="nav-logo">Codeways</h3>
+        </div>
+        <ul class="side-nav">
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    </>
+    
+  )
+}
+
+export default Navbar
