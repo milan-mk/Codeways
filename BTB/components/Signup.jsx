f7ac7bfc6b58c32b89babaeae8aac8167276a0df
milan-mk <milanmodak2005@gmail.com>
2025-01-17 00:42:10 +0530

    created signup page

--- BTB/components/Signup.jsx
+++ BTB/components/Signup.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import './Signup.css'
+import { Link } from 'react-router-dom'
+
 
 const Signup = () => {
   return (
     <>
       <div>
-        
+        <form className='form box' action="">
+          <div className='form-box'>
+            <input className='box-inp' type="text" placeholder='enter your username'/>
+          </div>
+          <div className='form-box'>
+            <input className='box-inp' type="email" placeholder='enter your email' />
+          </div>
+          <div className='form-box'>
+            <input className='box-inp' placeholder='enter your password' type="text" name="" id="" />
+          </div>
+          <div className='form-box'>
+            <span className='spn'>Already have an account? <Link to='/login' className='link'>Sign in</Link></span>
+          </div>
+          <div>
+            <button className='signup-btn'>Sign Up</button>
+          </div>
+          
+        </form>
       </div>
     </>
   )
