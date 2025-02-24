cee799b7ef5a9aafad1a1343a82461d3ddce821e
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-02-24 20:00:55 +0530

    Changed the copyrights.

--- BTB/components/Footer.jsx
+++ BTB/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer">
 
                 <p  >
-                    All &copy; Copyright reserved . Built and Maintained by team INIT
+                    All &copy; Copyright reserved . Built and Maintained by <a target='_blank' noreferrer href='https://www.linkedin.com/in/milan-kumar-modak-2782ab2b0/'>Milan Kumar Modak</a> and <a noreferrer target='_blank' href='https://www.linkedin.com/in/abhishek-sinha-3aa0a4293/'>Abhishek Sinha</a>
                 </p>
             </div>
         </>
