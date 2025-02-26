afc4c81d78002c6d5eb1f5423bb916a95aba1a8c
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-02-26 13:17:42 +0530

    created middleware for handling  errors

---
+++ Backend/middlewares/errorMiddleware.js
@@ -0,0 +1,36 @@
+class ErrorHandler extends Error{
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    if(err.statusCode === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = ErrorHandler(message, 400);
+    }
+
+    if(err.name === "JsonWebTokenError") {
+        const message = "Json Web Token is Invalid! Please Try again!";
+        err = ErrorHandler(message, 400);
+    }
+
+    if(err.name === "TokenExpiredError") {
+        const message = "Json Web Token is Invalid! Please Try again!";
+        err = ErrorHandler(message, 400);
+    }
+
+    if(err.name === "CastError") {
+        const message = `Invalid ${err.path}`;
+        err = ErrorHandler(message, 400);
+    }
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message : err.message,
+    });
+};
